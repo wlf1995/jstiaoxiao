b4f88a04426c23c9c2dbcad322cb7595195e1864
wanglifang <1181649564@qq.com>
2019-05-06 17:40:32 +0800

    first

---
+++ src/test/java/com/demo/chanyeditu/ChanyedituApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.chanyeditu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChanyedituApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
